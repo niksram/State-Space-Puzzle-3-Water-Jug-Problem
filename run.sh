28dacbfd17689e683246a0b0b63ba685a2889cf8
niksram <nikhilsram.off@gmail.com>
2020-03-22 19:51:02 +0530

    Final Commit

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 make
 make clean
-./a.out < input.txt > output.txt
-./a.out < input1.txt > output1.txt
-./a.out < input2.txt > output2.txt
-./a.out < input3.txt > output3.txt
-./a.out < input4.txt > output4.txt
-./a.out < input5.txt > output5.txt
-./a.out < input6.txt > output6.txt
+./a.out < input/input1.txt > output/output1.txt
+./a.out < input/input2.txt > output/output2.txt
+./a.out < input/input3.txt > output/output3.txt
+./a.out < input/input4.txt > output/output4.txt
+./a.out < input/input5.txt > output/output5.txt
+./a.out < input/input6.txt > output/output6.txt
+./a.out < input/input7.txt > output/output7.txt
+./a.out < input/input8.txt > output/output8.txt
+./a.out < input/input9.txt > output/output9.txt
+./a.out < input/input10.txt > output/output10.txt
+./a.out < input/input11.txt > output/output11.txt
+./a.out < input/input12.txt > output/output12.txt
